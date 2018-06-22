3b22d8da70e55ff6079a6f5f406510e0e5d80e6a
Ossi Herrala <oherrala@iki.fi>
2018-06-22 22:54:26 +0300

    Disable unreachable_pub lint for generated code

--- lib/pcap/bindings.rs
+++ lib/pcap/bindings.rs
@@ -6,4 +6,7 @@
 // We don't care about generated code's clippy nags
 #![cfg_attr(feature = "cargo-clippy", allow(clippy))]
 
+// More things we don't care about in generated code
+#![allow(unreachable_pub)]
+
 include!(concat!(env!("OUT_DIR"), "/pcap_bindings.rs"));
